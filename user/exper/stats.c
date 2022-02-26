275351093eb64a7c4994baa906bfa65d98041a62
Lorenzo Stoakes <lstoakes@gmail.com>
2022-02-26 18:28:12 +0000

    user: exper: abstract out field parsing

--- user/exper/stats.c
+++ user/exper/stats.c
@@ -23,52 +23,69 @@ struct mem_stats *get_mem_stats(struct mem_stats *stats)
 	return stats;
 }
 
-// Read smaps fields from `fp` pointer.
+// Attempt to parse smaps field from `buf` and place in appropriate field in
+// `stats`. If the format is not as expected or unable to parse, returns false,
+// otherwise returns true.
+static bool parse_smaps_field(const char *buf, struct smaps_rollup *stats)
+{
+	char prefix[256];
+	int val;
+	char kb[256];
+
+	if (sscanf(buf, "%s %d %s", prefix, &val, kb) == EOF ||
+	    strcmp(kb, "kB") != 0)
+		return false;
+
+#define GET_VALUE(_key, _field)                    \
+	if (strcmp(prefix, #_key ":") == 0) {	   \
+		stats->_field = val;		   \
+		return true;			   \
+	}
+
+	GET_VALUE(Rss, rss);
+	GET_VALUE(Pss, pss);
+	GET_VALUE(Pss_Anon, pss_anon);
+	GET_VALUE(Pss_File, pss_file);
+	GET_VALUE(Pss_Shmem, pss_shmem);
+	GET_VALUE(Shared_Clean, shared_clean);
+	GET_VALUE(Shared_Dirty, shared_dirty);
+	GET_VALUE(Private_Clean, private_clean);
+	GET_VALUE(Private_Dirty, private_dirty);
+	GET_VALUE(Referenced, referenced);
+	GET_VALUE(Anonymous, anonymous);
+	GET_VALUE(LazyFree, lazy_free);
+	GET_VALUE(AnonHugePages, anon_huge_pages);
+	GET_VALUE(ShmemPmdMapped, shmem_pmd_mapped);
+	GET_VALUE(FilePmdMapped, file_pmd_mapped);
+	GET_VALUE(Shared_Hugetlb, shared_hugetlb);
+	GET_VALUE(Private_Hugetlb, private_hugetlb);
+	GET_VALUE(Swap, swap);
+	GET_VALUE(SwapPss, swap_pss);
+	GET_VALUE(Locked, locked);
+#undef GET_VALUE
+
+	// Unknown field, ignore.
+	return true;
+}
+
+// Read smaps fields from `fp` stream into `stats`.
 static void read_smaps_rollup_fields(FILE *fp, struct smaps_rollup *stats)
 {
 	// Arbitrary sizes, we're into buffer overfow territory here...
-	char buf[256], kb[256];
-	int val;
+	char line[256];
 
-	// Skip first line.
-	if (fgets(buf, sizeof(buf), fp) == NULL) {
-		fprintf(stderr, "Cannot read first line.\n");
+	// Skip top line.
+	if (fgets(line, sizeof(line), fp) == NULL) {
+		fprintf(stderr, "Cannot read line.\n");
 		exit(EXIT_FAILURE);
 	}
 
-	while (fscanf(fp, "%s %d %s", buf, &val, kb) != EOF) {
-		// If we don't see 'kB' in this field, we aren't scanning fields
-		// any more.
-		if (strcmp(kb, "kB") != 0)
-			break;
-
-#define GET_VALUE(_key, _field)                    \
-		if (strcmp(buf, #_key ":") == 0) { \
-			stats->_field = val;       \
-			continue;                  \
+	// Read each line and extract fields.
+	while (fgets(line, sizeof(line), fp) != NULL) {
+		if (!parse_smaps_field(line, stats)) {
+			fprintf(stderr, "Cannot parse line: %s\n", line);
+			exit(EXIT_FAILURE);
 		}
-
-		GET_VALUE(Rss, rss);
-		GET_VALUE(Pss, pss);
-		GET_VALUE(Pss_Anon, pss_anon);
-		GET_VALUE(Pss_File, pss_file);
-		GET_VALUE(Pss_Shmem, pss_shmem);
-		GET_VALUE(Shared_Clean, shared_clean);
-		GET_VALUE(Shared_Dirty, shared_dirty);
-		GET_VALUE(Private_Clean, private_clean);
-		GET_VALUE(Private_Dirty, private_dirty);
-		GET_VALUE(Referenced, referenced);
-		GET_VALUE(Anonymous, anonymous);
-		GET_VALUE(LazyFree, lazy_free);
-		GET_VALUE(AnonHugePages, anon_huge_pages);
-		GET_VALUE(ShmemPmdMapped, shmem_pmd_mapped);
-		GET_VALUE(FilePmdMapped, file_pmd_mapped);
-		GET_VALUE(Shared_Hugetlb, shared_hugetlb);
-		GET_VALUE(Private_Hugetlb, private_hugetlb);
-		GET_VALUE(Swap, swap);
-		GET_VALUE(SwapPss, swap_pss);
-		GET_VALUE(Locked, locked);
-#undef GET_VALUE
 	}
 }
 
